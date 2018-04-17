71cb35e5bfea0bf8a92e93f2d2efadeece0ea2fa
Mangol <tritemy@yandex.ru>
2018-04-17 21:52:31 +0500

    Добавлен JS для модалки и карта

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+var modalPopup = document.querySelector(".modal-window-container");
+var writeUsButton = document.querySelector(".contacts__button");
+var closePopup = document.querySelector(".modal-close-button");
+
+// writeUsButton.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   modalPopup.classList.add("modal--show");
+// });
+//
+// closePopup.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   modalPopup.classList.remove("modal--show");
+// });
+
+writeUsButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalPopup.classList.remove("visually-hidden");
+});
+
+closePopup.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalPopup.classList.add("visually-hidden");
+});
+
+
